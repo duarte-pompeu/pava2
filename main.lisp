5301d27ea55d2200cb756ae0a519bd04126e942a
duarte-pompeu <pnt12@live.com.pt>
2017-04-28 13:06:07 +0100

    first commit

--- main.lisp
+++ main.lisp
@@ -20,17 +20,16 @@
 	(second class)
 )
 
+(defun get-obj-class (obj)
+	(first obj)
+)
+
+(defun get-attribs-hash (obj)
+	(second obj)
+)
+
 (defmacro def-class (class-name &rest body)
-	; class name as a string	
-	;~ (format t  "~S~%" (string-downcase (string class-name)))
-	
-	; loop each attrib
-	(loop for att in body
-		;~ do (format t "attribute: ~S~%" att)
-	)
-	
 	; splice all values into containing expression - this seems useful to define make-person (arg1 arg2 ... argn)
-	;~ (format t "~S~%" `(,@body))
 	(setq constructor-name (concatenate 'string "make-" (string-downcase (string class-name))))
 	;~ (format t "~S~%" constructor-name)
 	`(progn
@@ -45,9 +44,16 @@
 		; definir construtor
 		(defun ,(read-from-string constructor-name)	
 			(,@body) ; args
-			;~ (format t "~S: ~S ~%" ,(string-downcase (string class-name)) (list ,@body))
-			(list ,(get-class (string-downcase (string class-name)))
+			(list ,(string-downcase (string class-name)) ; FIXME: use symbol
 				(make-hash-table :test #'equal)))
+		
+		,(dolist (attrib body)
+			(format t "~S ~%" (concatenate 'string (string-downcase (string class-name)) "-" (string-downcase (string attrib))))
+			
+			`(defun ,(concatenate 'string (string-downcase (string class-name)) "-" (string-downcase (string attrib)))
+				(object)
+				(gethash ,(string-downcase (string attrib)) (get-attribs-obj obj)) ; TODO: check if working
+			))
 ))
 
 
