230e34da2bd661fcd625bda5fd921d3b3b638785
marciosan <mrc.snts@gmail.com>
2017-05-09 12:14:52 +0100

    added test for single inheritance

--- tests.lisp
+++ tests.lisp
@@ -27,8 +27,21 @@
 
 (define-test herança
 	(def-class (estudante pessoa) curso)
-	(make-estudante :nome "abc" :idade 25 :curso "leic")
-)
+	(let ((e (make-estudante :nome "abc" :idade 25 :curso "leic")))
+
+	(assert-equal "abc" (estudante-nome e))
+	(assert-equal 25 (estudante-idade e))
+	(assert-equal "leic" (estudante-curso e))
+))
+
+(define-test herança-duplicados
+	(def-class (aluno pessoa) nome curso)
+	(let ((a (make-aluno :nome "abc" :idade 25 :curso "leic")))
+
+	(assert-equal "abc" (aluno-nome a))
+	(assert-equal 25 (aluno-idade a))
+	(assert-equal "leic" (aluno-curso a))
+))
 
 (setq *print-failures* t)
 (setq *print-errors* t)
