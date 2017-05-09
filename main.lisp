a6ad643f3678531f6c9bce47bf000d46f0f62e91
marciosan <mrc.snts@gmail.com>
2017-05-09 11:54:18 +0100

    simple inheritance; not checking duplicated fields

--- main.lisp
+++ main.lisp
@@ -31,7 +31,8 @@
 (defmacro def-class (first-arg &rest body)	
 	(let* ((constructor-name nil)
 		(classname nil)
-		(existe-herança nil))
+		(existe-herança nil)
+		(class-fields nil))
 	
 	(if (listp first-arg)
 		(progn
@@ -47,7 +48,27 @@
 	
 	; splice all values into containing expression - this seems useful to define make-person (arg1 arg2 ... argn)
 	(setq constructor-name (concatenate 'string "make-" (string-downcase (string classname))))
-
+	
+	; check for inheritance
+	(if (not existe-herança)
+		(setq class-fields body) ; no inherited fields
+		(let ((superclass-attribs (get-class-attributes (get-class (string-downcase (string (second first-arg))))))
+			 (result nil))
+			 (format t "body ~S ~%" body)
+			 (format t "body fields ~S ~%" class-fields)
+			(format t "~S ~%" superclass-attribs)
+			(setq class-fields body)
+			(dolist (attrib superclass-attribs result)
+				(format t "attrib ~S ~%" attrib)
+				(push attrib result)
+			)
+			(format t "result ~S ~%" result)
+			(setq class-fields (concatenate 'list class-fields result))
+			(format t "all class fields ~S ~%" class-fields)
+		)		
+	)	
+	(format t "all class fields ~S ~%" class-fields)
+	
 	`(progn
 		; definir simbolo com meta informação da classe, por exemplo:
 		; >(def-class pessoa nome idade)
@@ -55,18 +76,18 @@
 		; >pessoa
 		; ("pessoa" '(NOME IDADE))
 		(set-class ,(string-downcase (string classname))  
-			(list ,(string-downcase (string classname)) '(,@body)))
+			(list ,(string-downcase (string classname)) '(,@class-fields)))
 		
 		; definir construtor
 		(defun ,(read-from-string constructor-name)	
-			(&key ,@body) ; args
+			(&key ,@class-fields) ; args
 			(let ((hashmap (make-hash-table :test #'equal)))
 				
 				; set fields
-				,@(mapcar #'(lambda (attrib) `(setf (gethash ,(string-downcase (string attrib)) hashmap) ,attrib)) body)
+				,@(mapcar #'(lambda (attrib) `(setf (gethash ,(string-downcase (string attrib)) hashmap) ,attrib)) class-fields)
 								
 				;~ ,@(let ((result nil))
-					;~ (dolist (attrib body result)
+					;~ (dolist (attrib class-fields result)
 						;~ (push `(setf (gethash ,(string-downcase (string attrib)) hashmap) ,attrib)
 							;~ result)))
 				
@@ -80,10 +101,10 @@
 			`(defun ,(read-from-string (concatenate 'string (string-downcase (string classname)) "-" (string-downcase (string attrib))))
 					(object)
 				(gethash ,(string-downcase (string attrib)) (get-attribs-hash object))))
-			body)
+			class-fields)
 		; old code for getters
 		;~ ,@(let ((result nil))
-			;~ (dolist (attrib body result)
+			;~ (dolist (attrib class-fields result)
 				;~ ;(format t "~S ~%" (concatenate 'string (string-downcase (string classname)) "-" (string-downcase (string attrib))))
 				;~ (push
 					;~ `(defun ,(read-from-string (concatenate 'string (string-downcase (string classname)) "-" (string-downcase (string attrib))))
@@ -97,6 +118,6 @@
 			`(defun ,(read-from-string (concatenate 'string "set-" (string-downcase (string classname)) "-" (string-downcase (string attrib))))
 					(object new-value)
 				(setf (gethash ,(string-downcase (string attrib)) (get-attribs-hash object)) new-value)))
-			body)
+			class-fields)
 	)
 ))
