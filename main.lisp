38e7c13c4b9632142defb845795b5fec60505322
duarte-pompeu <ist173008@lab14p8.rnl.ist.utl.pt>
2017-05-16 12:32:51 +0100

    remove commented code

--- main.lisp
+++ main.lisp
@@ -36,18 +36,13 @@
 	(let ((supers '())
 		(to-process (get-superclasses (get-class class)))
 		(current-class nil))
-		
-		;~ (format t "arg: ~S~%" class)
-		
+				
 		(loop while (not (null (first to-process)))
 		do (progn
 			(setq current-class (first to-process))
-			;~ (format t "current: ~S~%" current-class)
 			(setq to-process (rest to-process))
 			(push current-class supers)
-			(setq to-process (concatenate 'list to-process (get-superclasses (get-class (string-downcase (string current-class))))))))
-		
-		;~ (format t "supers: ~S ~%" supers)
+			(setq to-process (concatenate 'list to-process (get-superclasses (get-class (string-downcase (string current-class))))))))		
 supers))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -71,12 +66,10 @@ supers))
 	
 	(if (listp first-arg)
 		(progn
-			;~ (format t "com herança~%")
 			(setq classname (first first-arg))
 			(setq existe-herança t)
 		)
 		(progn 
-			;~ (format t "sem herança~%")
 			(setq classname first-arg)
 			(setq existe-herança nil)
 	))
@@ -91,15 +84,11 @@ supers))
 			 (result nil))
 			(setq class-fields body)
 			(dolist (superclass (rest first-arg) result)
-				;~ (format t "processing superclass ~S ~%" superclass)
 				(dolist (attrib (get-class-attributes (get-class (string-downcase (string superclass)))) result)
 					(if (not (member attrib class-fields)) (push attrib result))
 				)
-				;~ (format t "current result ~S ~%" result)
 			)
-			;~ (format t "result ~S ~%" result)
 			(setq class-fields (concatenate 'list class-fields result))
-			;~ (format t "all class fields ~S ~%" class-fields)
 	))
 	
 	; create class and put it in classes map
@@ -111,7 +100,6 @@ supers))
 				class-fields
 				(if existe-herança
 					(progn 
-					;~ (format t "rest: ~S~%" (rest first-arg))
 					(mapcar (lambda (x) (string-downcase (string x))) (rest first-arg)))
 					'()
 	)))
@@ -124,9 +112,8 @@ supers))
 				
 				; set fields
 				,@(mapcar #'(lambda (attrib) `(setf (gethash ,(string-downcase (string attrib)) hashmap) ,attrib)) class-fields)
-				; old code with do list (check deb5431a35121c3972a44d380e1a218b48ece3bb or previous)
 				
-				(list ,(string-downcase (string classname)) ; FIXME: use symbol
+				(list ,(string-downcase (string classname))
 					hashmap)))
 		
 		; generate getters
@@ -136,7 +123,6 @@ supers))
 					(object)
 				(gethash ,(string-downcase (string attrib)) (get-attribs-hash object))))
 			class-fields)
-		; old code for getters (check deb5431a35121c3972a44d380e1a218b48ece3bb or previous)
 		
 		; generate setters
 		,@(mapcar #'(lambda (attrib)
@@ -148,9 +134,6 @@ supers))
 		; generate recognizer
 		(defun ,(read-from-string (concatenate 'string (string-downcase (string classname)) "?"))
 			(object)
-			;~ (format t "obj: ~S~%" object)
-			;~ (format t "superclasses: ~S~%" (get-superclasses (get-class (get-obj-class object))))
-			;~ (format t "class: ~S~%" (read-from-string (get-obj-class object)))
 			
 			(cond 
 			; casos em que estutura de dados não corresponde aos nossos objectos
@@ -162,7 +145,6 @@ supers))
 				
 			; harder case: macro class == 1 of object superclasses
 			((member-if #'(lambda (x) (equal x ,(string-downcase (string classname)))) (get-all-superclasses (get-class-name object))) T)
-			;~ ((not (equal nil (member ',(read-from-string (string-downcase (string classname))) (get-all-superclasses (get-class (get-obj-class object)))))) T)
 			))
 	)
 ))
